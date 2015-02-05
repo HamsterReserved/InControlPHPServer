3b1047ac74aa3a5fc281d935470cdbabb402e1e6
Hamster Tian <haotia@gmail.com>
2015-02-05 23:34:47 +0800

    properly check of SAE environment

--- inccon_db.php
+++ inccon_db.php
@@ -1,5 +1,5 @@
 <?php
-    if (defined(IS_SAE)) {
+    if (defined("SAE_MYSQL_HOST_M")) {
         define("DB_HOST", SAE_MYSQL_HOST_M);
         define("DB_USERNAME", SAE_MYSQL_USER);
         define("DB_PASSWORD", SAE_MYSQL_PASS);
