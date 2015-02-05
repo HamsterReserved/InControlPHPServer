a06a293547c59af3a41d1d04430dd088b8e629cd
Hamster Tian <haotia@gmail.com>
2015-02-05 13:47:02 +0800

    complete database support

--- inccon_db.php
+++ inccon_db.php
@@ -6,25 +6,34 @@
     define("DB_NAME", "app_incontrol");
     define("CONTROL_CENTER_TBL_NAME", "control_centers");
     define("SENSOR_DATA_TBL_NAME", "sensor_data");
-    
+    define("SENSOR_INFO_TBL_NAME", "sensor_info");
+
     $CONTROL_CENTER_COLUMNS = array(
                         "machine_id" => "text", // Device ID
                         "man_date" => "int", // Manufacturing date
                         "reg_date" => "int", // First time registered (user activation)
+                        "name" => "text", // Device name defined by user
+                        "cred_md5" => "text", // User credentials, MD5 of course
                         "clients" => "text", // Allowed client IDs
                         "triggers" => "text", // Send SMS etc
                         "state" => "tinyint", // Will I accept new clients/sensors? 0=normal 1=new clients 2=new sensors
                         "last_state_date" => "int" // Reject new clients/sensors after 2mins
                         );
-    define("CONTROL_CENTER_PRIMARY_KEY", "row_id");
-    
+    define("CONTROL_CENTER_PRIMARY_KEY", "machine_id");
+
     $SENSOR_DATA_COLUMNS = array(
-                        "sensor_id" => "text",
-                        "sensor_type" => "int",
-                        "sensor_name" => "text",
-                        "sensor_date" => "int",
-                        "sensor_value" => "int",
-                        "machine_id" => "text" // Paired host machine
+                        "data_row_id" => "text", // Same as SENSOR_INFO
+                        "date" => "int",
+                        "value" => "int",
                         );
-    define("SENSOR_DATA_PRIMARY_KEY", "row_id");
+    define("SENSOR_DATA_PRIMARY_KEY", "sensor_id"); // Mainly search by this
+
+    $SENSOR_INFO_COLUMNS = array(
+                        "row_id" => "int NOT NULL AUTO_INCREMENT",
+                        "sensor_id" => "text",
+                        "type" => "int",
+                        "name" => "text",
+                        "assoc_machine_id" => "text" // Paired host machine
+                        );
+    define("SENSOR_INFO_PRIMARY_KEY", "assoc_machine_id"); // Mainly search by this
 ?>
--- incontrol_db.php
+++ incontrol_db.php
@@ -14,24 +14,22 @@
             $this->check_mysqli_err();
         }
 
+        function create_tables_for_array($table_name, $arr) {
+            $sql = "CREATE TABLE IF NOT EXISTS " . 
+                    $table_name . 
+                    "(" . 
+                    implode(", ", array_combine_key_value($arr, " ")) .
+                    ")";
+            $this->mysqli->query($sql);
+            $this->check_mysqli_err();
+        }
+
         function create_tables() {
-            global $CONTROL_CENTER_COLUMNS, $SENSOR_DATA_COLUMNS;
+            global $CONTROL_CENTER_COLUMNS, $SENSOR_DATA_COLUMNS, $SENSOR_INFO_COLUMNS;
 
-            $sql = "CREATE TABLE IF NOT EXISTS " . 
-                    CONTROL_CENTER_TBL_NAME . 
-                    "(" . 
-                    implode(", ", array_combine_key_value($CONTROL_CENTER_COLUMNS, " ")) .
-                    ")";
-            $this->mysqli->query($sql);
-            $this->check_mysqli_err();
-
-            $sql = "CREATE TABLE IF NOT EXISTS " . 
-                    SENSOR_DATA_TBL_NAME . 
-                    "(" . 
-                    implode(", ", array_combine_key_value($SENSOR_DATA_COLUMNS, " ")) .
-                    ")";
-            $this->mysqli->query($sql);
-            $this->check_mysqli_err();
+            create_tables_for_array(CONTROL_CENTER_TBL_NAME, $CONTROL_CENTER_COLUMNS);
+            create_tables_for_array(SENSOR_DATA_TBL_NAME, $SENSOR_DATA_COLUMNS);
+            create_tables_for_array(SENSOR_INFO_TBL_NAME, $SENSOR_INFO_COLUMNS);
         }
 
         function check_mysqli_err() {
@@ -46,6 +44,79 @@
                             "DB Operation Failed with ", 
                             __FUNCTION__, 
                             "error " . $this->mysqli->error . " (" . $this->mysqli->errno . ")");
+            }
+        }
+
+        function add_new_device($machine_id) { // For manufacturing
+            // This is rare, no need to use prepared statment. Am I lazy?
+            $machine_id = $mysqli->real_escape_string($machine_id);
+            $sql = "INSERT INTO " . CONTROL_CENTER_TBL_NAME . " (machine_id, man_date) VALUES ('$machine_id', " . time() . ")";
+            $this->mysqli->query($sql);
+            $this->check_mysqli_err();
+        }
+
+        function register_device($machine_id, $device_name) { // For users first use of a device
+            $machine_id = $mysqli->real_escape_string($machine_id);
+            $device_name = $mysqli->real_escape_string($device_name);
+            $sql = "UPDATE " . CONTROL_CENTER_TBL_NAME . " SET name = '$device_name', reg_date = " . time() . " WHERE machine_id = '$machine_id'";
+            $this->mysqli->query($sql);
+            $this->check_mysqli_err(); // TODO Shall we expose machine_id not found error to normal user?
+        }
+
+        function set_device_name($machine_id, $device_name) { // Nearly same as above, except we don't set reg_date
+            $machine_id = $mysqli->real_escape_string($machine_id);
+            $device_name = $mysqli->real_escape_string($device_name);
+            $sql = "UPDATE " . CONTROL_CENTER_TBL_NAME . " SET name = '$device_name' WHERE machine_id = '$machine_id'";
+            $this->mysqli->query($sql);
+            $this->check_mysqli_err();
+        }
+
+        function update_sensor_value($sensor_id, $machine_id, $sensor_type, $sensor_value) { // Register and update are in one function
+            if (!is_numeric($sensor_type) || !is_numeric($sensor_value)) {
+                ensure_not_null(NULL, "sensor_type and sensor_value", __FUNCTION__, "should be integer!");
+            }
+
+            $sensor_id = $mysqli->real_escape_string($sensor_id);
+            $machine_id = $mysqli->real_escape_string($machine_id);
+            $sensor_type = $mysqli->real_escape_string($sensor_type);
+            $sensor_value = $mysqli->real_escape_string($sensor_value);
+
+            $check_sql = "SELECT row_id FROM " . SENSOR_INFO_TBL_NAME . " WHERE sensor_id='$sensor_id' AND assoc_machine_id='$machine_id'";
+            $check_sql_result = $this->mysqli->query($check_sql);
+            if ($this->mysqli->affected_rows == 0) { // This sensor hasn't been registered yet, register now.
+                $create_sql = "INSERT INTO " . SENSOR_INFO_COLUMNS . " (sensor_id, assoc_machine_id, type, value) VALUES " .
+                        "('$sensor_id', '$machine_id', $sensor_type, $sensor_value)";
+                $this->mysqli->query($create_sql);
+                $this->check_mysqli_err();
+                $row_id = $this->mysqli->insert_id;
+            } else if($this->mysqli->affected_rows >= 1) { // Already registered. >1 is abnormal but we can't do anything... for now
+                $result_row = $check_sql_result->fetch_row(); // Only first row
+                $row_id = $result_row[0];
+            }
+            $check_sql_result->close();
+
+            $insert_data_sql = "INSERT INTO " . SENSOR_DATA_TBL_NAME . " (data_row_id, date, value) VALUES ".
+                        "($row_id, " . time() . ", $sensor_value)";
+            $this->mysqli->query($insert_data_sql);
+            $this->check_mysqli_err();
+        }
+
+        function set_sensor_name($sensor_id, $machine_id, $new_name) {
+            $sensor_id = $mysqli->real_escape_string($sensor_id);
+            $machine_id = $mysqli->real_escape_string($machine_id);
+
+            $check_sql = "SELECT row_id FROM " . SENSOR_INFO_TBL_NAME . " WHERE sensor_id='$sensor_id' AND assoc_machine_id='$machine_id'";
+            $check_sql_result = $this->mysqli->query($check_sql);
+            if ($this->mysqli->affected_rows == 0) { // This sensor hasn't been registered yet, DIE!
+                $check_sql_result->close();
+                ensure_not_null(NULL, "This sensor hasn't been registered!", __FUNCTION__, "");
+            } else if ($this->mysqli->affected_rows >= 1) { // Already registered. >1 is abnormal but we can't do anything... for now
+                $result_row = $check_sql_result->fetch_row(); // Only first row
+                $row_id = $result_row[0];
+                $check_sql_result->close();
+                $update_sql = "UPDATE " . SENSOR_INFO_TBL_NAME . " SET name = '$new_name' WHERE row_id = $row_id";
+                $this->mysqli->query($update_sql);
+                $this->check_mysqli_err();
             }
         }
     }
